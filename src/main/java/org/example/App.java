19a60e59b6b8e00a4a297eeff197756098f380a1
Test <Test@test.com>
2021-09-13 22:04:13 -0400

    Added assignment code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.util.Objects;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 15 Solution
+ *  Copyright 2021 Tyler Coleman
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String knownPassword = "abc$123";
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your username? ");
+        String user = in.nextLine();
+        System.out.println("What is your password? ");
+        String pass = in.nextLine();
+        if (Objects.equals(pass, knownPassword)) {
+            System.out.println("Welcome!");
+        }
+        else {
+            System.out.println("I don't know you. What are you doing in my house?");
+        }
     }
 }
